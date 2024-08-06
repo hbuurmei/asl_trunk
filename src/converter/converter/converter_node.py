0842d25d33065407f0529cfb9d13c265a5e9320d
hbuurmei <hugo.buurmeijer@gmail.com>
2024-08-06 15:24:49 -0700

    Publish rigid_bodies as well

--- src/converter/converter/converter_node.py
+++ src/converter/converter/converter_node.py
@@ -1,17 +1,30 @@
 import rclpy
 from rclpy.node import Node
-from interfaces.msg import TrunkMarkers
-from mocap4r2_msgs.msg import Markers
+from interfaces.msg import TrunkMarkers, TrunkRigidBodies
+from mocap4r2_msgs.msg import Markers, RigidBodies
 
 class ConverterNode(Node):
     def __init__(self):
         super().__init__('converter_node')
-        self.publisher = self.create_publisher(TrunkMarkers, '/trunk_markers', 10)
-        self.subscription = self.create_subscription(
-            Markers,
-            '/markers',
-            self.marker_callback,
-            10)
+
+        self.declare_parameter('type', 'rigid_bodies')
+        self.type = self.get_parameter('type').get_parameter_value().string_value
+
+        if self.type == 'rigid_bodies':
+            self.publisher = self.create_publisher(TrunkRigidBodies, '/trunk_rigid_bodies', 10)
+            self.subscription = self.create_subscription(
+                RigidBodies,
+                '/rigid_bodies',
+                self.rigid_body_callback,
+                10)
+        
+        elif self.type == 'markers':
+            self.publisher = self.create_publisher(TrunkMarkers, '/trunk_markers', 10)
+            self.subscription = self.create_subscription(
+                Markers,
+                '/markers',
+                self.marker_callback,
+                10)
         self.get_logger().info('Mocap converter node has started.')
 
     def marker_callback(self, msg):
@@ -21,6 +34,16 @@ class ConverterNode(Node):
         trunk_msg.translations = [marker.translation for marker in msg.markers]
         self.publisher.publish(trunk_msg)
         # self.get_logger().info('Published TrunkMarkers message.')
+
+    def rigid_body_callback(self, msg):
+        trunk_msg = TrunkRigidBodies()
+        trunk_msg.header = msg.header
+        trunk_msg.frame_number = msg.frame_number
+        trunk_msg.rigid_body_names = [rigid_body.rigid_body_name for rigid_body in msg.rigidbodies]
+        trunk_msg.positions = [rigid_body.pose.position for rigid_body in msg.rigidbodies]
+        self.publisher.publish(trunk_msg)
+        # self.get_logger().info('Published TrunkRigidBodies message.')
+
 
 def main(args=None):
     rclpy.init(args=args)
